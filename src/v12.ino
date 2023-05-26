b963981ac85e8b45e9bf02c4caa4548ed8bf1dcd
MostafaHendy3 <mostafahendy9991@gmail.com>
2023-05-26 03:57:04 +0200

    last Change

--- src/v12.ino
+++ src/v12.ino
@@ -7,12 +7,14 @@
 #define fan 5
 #define btn 1
 #define buzzer 6
-#define ir1 A1
-#define ir2 A2
-#define servo 9
+#define ir1 13
+#define ir2 8
+#define servo 10
 #define ldr A3
-#define led1 10
+#define led5 9
 #define led2 11
+#define led3 7
+#define led4 12
 #define TemperatureSensor A0
 
 // Define the rows and columns of the keypad
@@ -21,8 +23,8 @@ const byte COLS = 2;
 
 // Define the keymap
 char keys[ROWS][COLS] = {
-    {'4', '3'},
-    {'1', '2'}};
+    {'2', '4'},
+    {'1', '3'}};
 
 // Define the pins connected to the keypad rows and columns
 byte rowPins[ROWS] = {0, 1};
@@ -42,9 +44,12 @@ int reading;
 float volt;
 float temp;
 Servo Serv;
-int btnRead=0;
+int btnRead = 0;
 int ldrValue;
-int ledValue ;
+int ledValue1;
+int ledValue2;
+int currentAngle = 90;
+
 void setup()
 {
   // lcd initilizeing
@@ -52,24 +57,26 @@ void setup()
   lcd.backlight();
   lcd.clear();
   lcd.setCursor(0, 0);
-  lcd.print("Enter11 Password");
+  lcd.print("Enter Password");
 
   // servo
-  Serv.attach(servo);
   pinMode(servo, OUTPUT);
-  digitalWrite(servo, LOW);
-
+  Serv.attach(servo);
+  stopDoor();
   // ldr
   pinMode(ldr, INPUT);
   // map the LDR value to LED brightness value
-  
+
   // ir
   pinMode(ir1, INPUT);
   pinMode(ir2, INPUT);
 
   // led
-  pinMode(led1,OUTPUT);
-  pinMode(led2,OUTPUT);
+  pinMode(led5, OUTPUT);
+  analogWrite(led5, 0);
+  pinMode(led2, OUTPUT);
+  pinMode(led3, OUTPUT);
+  pinMode(led4, OUTPUT);
 
   // fan
   pinMode(fan, OUTPUT);
@@ -77,7 +84,7 @@ void setup()
 
   // btn
   pinMode(btn, INPUT);
-  
+
   // buzzer
   pinMode(buzzer, OUTPUT);
   digitalWrite(buzzer, LOW);
@@ -90,44 +97,64 @@ void setup()
   // check password untill it is correct
   while (checkPassword(ReadingPass) == 0)
   {
+
     lcd.clear();
     lcd.print("wrong Password");
     lcd.setCursor(0, 1);
+    digitalWrite(led4, HIGH);
+    digitalWrite(buzzer, HIGH);
     ReadingPassWord();
   }
   lcd.clear();
   lcd.print("Welcome Home");
+  digitalWrite(led3, HIGH);
+  digitalWrite(led4, LOW);
+
   delay(100);
 }
 
 void loop()
 {
-  //door system
-  if (digitalRead(ir1) == HIGH)
+  int ir1value = digitalRead(ir1);
+  int ir2value = digitalRead(ir2);
+
+  lcd.setCursor(0, 0);
+  lcd.print(ir1value);
+  // door system
+  if (ir1value != HIGH)
   {
     openDoor(180);
-  }else if (digitalRead(ir2) == HIGH)
+    delay(500);
+    stopDoor();
+  }else if(ir2value != HIGH)
   {
     openDoor(0);
-  }else
-  {
-    closeDoor();
+    delay(500);
+    stopDoor();
+  }else{
+    openDoor(90);
+    delay(500);
+    stopDoor();
   }
   delay(100);
 
   lcd.setCursor(0, 0);
+  lcd.clear();
+  ldrValue = analogRead(ldr);
+  ledValue1 = map(ldrValue, 0, 1023, 0, 255);
+  ledValue2 = map(ldrValue, 0, 1023, 0, 255);
   TempDisplay();
   // control led with ldr
-  ldrValue = analogRead(ldr); // read the value from the LDR
-  ledValue = map(ldrValue, 0, 1023, 0, 255);
-  analogWrite(led1, ledValue); 
-  analogWrite(led2, ledValue); 
-  //check btn reading
-  //temperature check with fan
+   // read the value from the LDR
+  
+  
+  // check btn reading
+  // temperature check with fan
   if (temp > 35)
   {
     digitalWrite(fan, LOW);
-    if(btnRead==0){
+    if (btnRead == 0)
+    {
       digitalWrite(buzzer, HIGH);
     }
   }
@@ -141,18 +168,39 @@ void loop()
     digitalWrite(fan, LOW);
   }
 }
-void BuzzerOff(){
-digitalWrite(buzzer, LOW);
-btnRead =1;
-}
-void openDoor(int f)
+void BuzzerOff()
 {
-  Serv.write(f);
+  digitalWrite(buzzer, LOW);
+  btnRead = 1;
+}
+void openDoor(int angle)
+{
+  if (angle < 0) {
+    angle = 0;
+  } else if (angle > 180) {
+    angle = 180;
+  }
+  if (angle > currentAngle) {
+    for (int i = currentAngle; i <= angle; i++) {
+      Serv.write(i);
+      delay(15); // Adjust the delay time for desired speed
+    }
+  } else if (angle < currentAngle) {
+    for (int i = currentAngle; i >= angle; i--) {
+      Serv.write(i);
+      delay(15); // Adjust the delay time for desired speed
+    }
+  }
+   currentAngle = angle;
+}
+void openDoorout()
+{
+  Serv.write(0);
   delay(100);
 }
-void closeDoor()
+void stopDoor()
 {
-  Serv.write(90);
+  Serv.write(currentAngle);
   delay(100);
 }
 void ReadingPassWord()
@@ -187,11 +235,37 @@ void TempDisplay()
 {
   lcd.setCursor(0, 0);
   reading = analogRead(TemperatureSensor);
-  lcd.print("Temperature");
+  lcd.print("Temp=");
   temp = reading * 500.0 / 1024;
   lcd.print(temp);
-  lcd.setCursor(0, 1);
-  float led = (ledValue/255.0)*100.0;
 
-  lcd.print(String(led)+"%");
+  lcd.setCursor(0, 1);
+  ////Brightness
+  //lcd.print(ldrValue);
+  if (ldrValue < 10)
+  {
+    lcd.print(" - Dark ");    
+    analogWrite(led5,255);
+  analogWrite(led2,255);
+  }
+  else if (ldrValue < 200)
+  {
+    lcd.print(" - Dim ");
+    analogWrite(led5,200);
+  analogWrite(led2,200);
+  }
+  else if (ldrValue < 600)
+  {
+    lcd.print(" - Light ");
+    analogWrite(led5,50);
+    analogWrite(led2,50);
+  }
+  else if (ldrValue < 1023)
+  {
+    lcd.print(" - Bright ");
+    analogWrite(led5,0);
+    analogWrite(led2,0);
+  }
+  
+  //lcd.print(ledValue);
 }
